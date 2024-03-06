805d5422f285e27aced7d0e346e8ce2fbf12acb7
Marina_Bsv <marky.law@yandex.ru>
2024-03-06 22:25:06 +0300

    Mesto

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,27 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Grogu",
+      link: "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Batman",
+      link: "https://images.unsplash.com/photo-1610568781018-995405522539?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "John Wick",
+      link: "https://images.unsplash.com/photo-1560354508-468e7201bbc2?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Star Wars",
+      link: "https://images.unsplash.com/photo-1472457897821-70d3819a0e24?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Harry Potter",
+      link: "https://images.unsplash.com/photo-1629944970361-d9fc34988644?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Thor",
+      link: "https://images.unsplash.com/photo-1626209731723-4bf514fcdcd1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const contentCards = document.querySelector(".content");
+const listCards = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: Функция создания карточки
+function addCards(data, deleteCard) {
+  const cardsElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const deleteButton = cardsElement.querySelector(".card__delete-button");
+  cardsElement.querySelector(".card__image").src = data.link;
+  cardsElement.querySelector(".card__image").alt = data.name;
+  cardsElement.querySelector(".card__title").textContent = data.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(deleteButton);
+  });
+
+  return cardsElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(deleteButton) {
+  const deleting = deleteButton.closest(".places__item");
+  deleting.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((data) => {
+  const card = addCards(data, deleteCard);
+  listCards.append(card);
+});
